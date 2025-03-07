079ca4f4fcd36d4404724446d8e9e7a94a83f5b3
Daniel Nogueira <dnogueira@ipca.pt>
2025-03-07 16:06:40 +0000

    Update na pasta Sokoban

---
+++ FirstProject/Sokoban/Game1.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Sokoban;
+
+public class Game1 : Game
+{
+    private GraphicsDeviceManager _graphics;
+    private SpriteBatch _spriteBatch;
+
+    private SpriteFont font; // Variavel de fonte de texto
+
+
+    public Game1()
+    {
+        _graphics = new GraphicsDeviceManager(this);
+        Content.RootDirectory = "Content";
+        IsMouseVisible = true;
+    }
+
+    protected override void Initialize()
+    {
+        // TODO: Add your initialization logic here
+
+        base.Initialize();
+    }
+
+    protected override void LoadContent()
+    {
+        _spriteBatch = new SpriteBatch(GraphicsDevice);
+        
+        font = Content.Load<SpriteFont>("File"); //Use the name of sprite font file ('File')
+
+
+        // TODO: use this.Content to load your game content here
+    }
+
+    protected override void Update(GameTime gameTime)
+    {
+        if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+            Exit();
+
+        // TODO: Add your update logic here
+
+        base.Update(gameTime);
+    }
+
+    protected override void Draw(GameTime gameTime)
+    {
+        GraphicsDevice.Clear(Color.CornflowerBlue);
+
+        // TODO: Add your drawing code here
+        _spriteBatch.Begin();
+        _spriteBatch.DrawString(font, "O texto que quiser", new Vector2(100, 100), Color.White);
+        _spriteBatch.DrawString(font, "O texto que quiser", new Vector2(100, 300), Color.Black);
+        _spriteBatch.End();
+       
+        base.Draw(gameTime);
+    }
+}
