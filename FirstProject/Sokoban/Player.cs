c71306ca4ea209f6e736c06ec2e53e5330db6736
Daniel Nogueira <dnogueira@ipca.pt>
2025-03-16 08:54:37 +0000

    Alterado o Player.cs

--- FirstProject/Sokoban/Player.cs
+++ FirstProject/Sokoban/Player.cs
@@ -14,7 +14,7 @@ namespace Sokoban
 
         private Point position; //Point = Vector2, mas são inteiros
         public Point Position => position; //auto função (equivalente a ter só get sem put) - AUTOPROPERTY
- 
+
         private Game1 game; //reference from Game1 to Player
         private bool keysReleased = true;
 
@@ -23,39 +23,10 @@ namespace Sokoban
         //	get{return position;}
         //}
 
-        public Player(Game1 game, int x, int y) //constructor que dada a as posições guarda a sua posição
+        public Player(Game1 game1, int x, int y) //constructor que dada a as posições guarda a sua posição
         {
             position = new Point(x, y);
-            Point lastPosition = position;
-
-            // destino é caixa?
-            if (game.HasBox(position.X, position.Y))
-            {
-                int deltaX = position.X - lastPosition.X;
-                int deltaY = position.Y - lastPosition.Y;
-                Point boxTarget = new Point(deltaX + position.X, deltaY + position.Y);
-                //  se sim, caixa pode mover-se?
-                if (game.FreeTile(boxTarget.X, boxTarget.Y))
-                {
-                    for (int i = 0; i < game.boxes.Count; i++)
-                    {
-                        if (game.boxes[i].X == position.X && game.boxes[i].Y == position.Y)
-                        {
-                            game.boxes[i] = boxTarget;
-                        }
-                    }
-                }
-                else
-                {
-                    position = lastPosition;
-                }
-            }
-            else
-            {
-                //  se não é caixa, se não está livre, parado!
-                if (!game.FreeTile(position.X, position.Y))
-                    position = lastPosition;
-            }
+            game = game1;
         }
 
         public void Update(GameTime gameTime)
@@ -63,24 +34,56 @@ namespace Sokoban
             KeyboardState kState = Keyboard.GetState();
             if (keysReleased)
             {
+                Point lastPosition = position;
                 keysReleased = false;
                 if (kState.IsKeyDown(Keys.A)) position.X--;
                 else if (kState.IsKeyDown(Keys.W)) position.Y--;
                 else if (kState.IsKeyDown(Keys.S)) position.Y++;
                 else if (kState.IsKeyDown(Keys.D)) position.X++;
                 else keysReleased = true;
+
+                // destino é caixa?
+                if (game.HasBox(position.X, position.Y))
+                {
+                    //     _ # Y 
+                    // Y = x0 = 10
+                    // # = x1 = 9
+                    // delta = x1 - x0 = -1
+                    // _ = x1 + delta
+                    int deltaX = position.X - lastPosition.X;
+                    int deltaY = position.Y - lastPosition.Y;
+                    Point boxTarget = new Point(deltaX + position.X, deltaY + position.Y);
+                    //  se sim, caixa pode mover-se?
+                    if (game.FreeTile(boxTarget.X, boxTarget.Y))
+                    {
+                        for (int i = 0; i < game.boxes.Count; i++)
+                        {
+                            if (game.boxes[i].X == position.X && game.boxes[i].Y == position.Y)
+                            {
+                                game.boxes[i] = boxTarget;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        position = lastPosition;
+                    }
+                }
+                else
+                {
+                    //  se não é caixa, se não está livre, parado!
+                    if (!game.FreeTile(position.X, position.Y))
+                        position = lastPosition;
+                }
             }
             else
             {
                 if (kState.IsKeyUp(Keys.A) && kState.IsKeyUp(Keys.W) &&
                     kState.IsKeyUp(Keys.S) && kState.IsKeyUp(Keys.D))
-                   
                 {
                     keysReleased = true;
                 }
             }
-
         }
-
     }
 }
--- FirstProject/Sokoban/obj/Debug/net6.0-windows/Sokoban.AssemblyInfo.cs
+++ FirstProject/Sokoban/obj/Debug/net6.0-windows/Sokoban.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Sokoban")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+33f4f075742afbf575e99513738cd226ee7ab1c4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bfeabfe8f3c09b17f028e39f6316a68eb264f55e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Sokoban")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Sokoban")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
